01fd6da66b896b9e9ea3fd5297816dd91e1ebe50
Abdallah Samy <abdallhsamy2011@gmail.com>
2022-06-23 21:38:34 +0200

    protect routes with auth guard

--- routes/web.php
+++ routes/web.php
@@ -22,31 +22,31 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 
-Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder')->name('io_generator_builder');
-
-Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate')->name('io_field_template');
-
-Route::get('relation_field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@relationFieldTemplate')->name('io_relation_field_template');
-
-Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate')->name('io_generator_builder_generate');
-
-Route::post('generator_builder/rollback', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@rollback')->name('io_generator_builder_rollback');
-
-Route::post(
-    'generator_builder/generate-from-file',
-    '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generateFromFile'
-)->name('io_generator_builder_generate_from_file');
-
-Route::resource('customers', App\Http\Controllers\CustomerController::class);
+//Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder')->name('io_generator_builder');
+//
+//Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate')->name('io_field_template');
+//
+//Route::get('relation_field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@relationFieldTemplate')->name('io_relation_field_template');
+//
+//Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate')->name('io_generator_builder_generate');
+//
+//Route::post('generator_builder/rollback', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@rollback')->name('io_generator_builder_rollback');
+//
+//Route::post(
+//    'generator_builder/generate-from-file',
+//    '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generateFromFile'
+//)->name('io_generator_builder_generate_from_file');
 
 
-Route::resource('productCategories', App\Http\Controllers\ProductCategoryController::class);
+Route::middleware('auth:web')->group(function() {
 
+    Route::resource('customers', App\Http\Controllers\CustomerController::class);
 
-Route::resource('products', App\Http\Controllers\ProductController::class);
+    Route::resource('productCategories', App\Http\Controllers\ProductCategoryController::class);
 
+    Route::resource('products', App\Http\Controllers\ProductController::class);
 
-Route::resource('sells', App\Http\Controllers\SellController::class);
+    Route::resource('sells', App\Http\Controllers\SellController::class);
 
-
-Route::resource('installments', App\Http\Controllers\InstallmentController::class);
+    Route::resource('installments', App\Http\Controllers\InstallmentController::class);
+});
